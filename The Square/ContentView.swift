3cbd4f24acae2cbf00af4a980cfda665e3f625d6
Ethan McHugh <ethanmchugh2024@gmail.com>
2022-08-01 13:30:56 -0500

    Added the things that Ali had

--- The Square/ContentView.swift
+++ The Square/ContentView.swift
@@ -2,13 +2,14 @@
 //  ContentView.swift
 //  The Square
 //
-//  Created by Ali on 01/08/2022.
+//  Created by Ali and Ethan on 01/08/2022.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     @State var score = 0
+    @State var lives = 3
     @State var x = 100.0
     @State var y = 100.0
     @State var time = 30
@@ -21,21 +22,31 @@ struct ContentView: View {
     
     
     var body: some View {
-        Text("Tap the square")
-            .padding()
+        VStack{
+            Text("Tap the square")
+            RoundedRectangle(cornerRadius: 0)
+                .frame(width: 100, height: 100, alignment: .center)
+                .position(x: x, y: y)
+                .foregroundColor(selectedColor)
+                .onTapGesture {
+                    selectedColor = getRandomColor()
+                    score += 1
+                    x = Double.random(in: 100...(maxX - 100))
+                    y = Double.random(in: 100...(maxY - 200))
+                }
+        }
+        
+        HStack{
         Text("Score: \(score)")
             .font(.title)
             .fontWeight(.bold)
-        RoundedRectangle(cornerRadius: 0)
-            .frame(width: 100, height: 100, alignment: .center)
-            .position(x: x, y: y)
-            .foregroundColor(selectedColor)
-            .onTapGesture {
-                selectedColor = getRandomColor()
-                score += 1
-                x = Double.random(in: 100...(maxX - 100))
-                y = Double.random(in: 100...(maxY - 200))
-            }
+            .position(x: 70, y: 300)
+        Text("Lives: \(lives)")
+                .font(.title)
+                .fontWeight(.bold)
+                .position(x: 120, y: 300)
+                .padding(2)
+        }
     }
     func getRandomColor() -> Color {
          let red:CGFloat = CGFloat(drand48())
