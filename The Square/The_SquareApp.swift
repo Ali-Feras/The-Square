10c8e59e8f178cf3c371a3dca4fdc54e36b051ba
Ali Feras <ferasali876@gmail.com>
2022-08-01 10:13:51 -0500

    Initial Commit

---
+++ The Square/The_SquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_SquareApp.swift
+//  The Square
+//
+//  Created by Ali on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct The_SquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
